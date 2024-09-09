32d4f86b4c6d5810e7d07b494c8e9d0af5bfdc60
Hussein Samy <82336571+HusseinSamy@users.noreply.github.com>
2024-09-09 16:46:58 +0300

    Adds invoices initial page

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">Invoices</h1>
+            <p className="mt-4">This is the invoices page</p>
+        </div>
+    )
+}
